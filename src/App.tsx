c501d9dba4f4e86227d8794fe9007a0210033a78
Davina Leong <davina.leong.sy@gmail.com>
2025-05-09 16:50:16 +0800

    Able to toggle the Password field type

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,53 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react"
+import "./App.css"
+
+const TYPE_PASSWORD: string = "password"
+const TYPE_TEXT: string = "text"
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [passwordFieldType, setPasswordFieldType] = useState(TYPE_PASSWORD)
+
+  const togglePasswordHandler = () => {
+    if (passwordFieldType === TYPE_PASSWORD) {
+      setPasswordFieldType(TYPE_TEXT)
+    } else {
+      setPasswordFieldType(TYPE_PASSWORD)
+    }
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <main className="grid place-items-center h-screen bg-slate-100">
+      <form action="" method="post">
+        <h1 className="text-4xl">Password Strength Meter</h1>
+
+        <div className="grid mt-4">
+          <label htmlFor="input-password" className="text-lg">
+            Please enter your password:
+          </label>
+          <input
+            type={passwordFieldType}
+            name="password"
+            id="input-password"
+            className="text-lg"
+          />
+          <div className="password-meter bg-green-800 h-0.5"></div>
+        </div>
+        <div>
+          <label
+            htmlFor="input-toggle_password"
+            className="flex gap-2 items-center"
+          >
+            <input
+              type="checkbox"
+              name="toggle_password"
+              id="input-toggle_password"
+              onClick={togglePasswordHandler}
+            />
+            Toggle Password
+          </label>
+        </div>
+      </form>
+    </main>
   )
 }
 
